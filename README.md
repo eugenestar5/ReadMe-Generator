89eb346c8460bf1e890defab149592c79258a887
eugenestar5 <eugene.bonsu@gmail.com>
2023-05-08 20:39:40 -0600

    adding all the files to the project

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# ReadMe-Generator
+# README GENERATOR
+ 
+
+ ## Table of Contents
+ - Description
+ - Installation
+ - Usage
+ - License
+ - Contributing
+ - Tests
+ - Questions
+ - Credits
+
+ ## Description
+It helps users to generate a ReadMe file for their project.
+
+ ## Installation
+Run NPM install to create modules and run the js file using node and answer all the questions on the command line.
+The ReadMe file, after it generates, get saved in the ReadMeGeneratorFolder.
+
+ ## Usage
+ It creates a ReadMe file for the user's project.
+
+## License
+MIT License
+
+    Copyright (c) [year] [fullname]
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+## Contributing
+N/A
+
+## Tests
+N/A
+
+## Questions
+N/A
+
+## Credits
+N/A
+
+
+NB: Link to recorded video
+https://drive.google.com/file/d/1oFFrCutEQY3YYvZ4j-sLT6_AvXQgjUPf/view
+
+
---
+++ index.js
@@ -0,0 +1,85 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkDown = require('./utils/generateMarkdown.js')
+const path = require("path")
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'Title'
+    },
+
+    {
+        type: 'input',
+        message: 'Please describe your project?',
+        name: 'Description'
+    },
+
+    {
+        type: 'input',
+        message: 'How can you install this project?',
+        name: 'Installation'
+    },
+
+    {
+        type: 'input',
+        message: 'How can this web application be used?',
+        name: 'Usage'
+    },
+
+    {
+        type: 'list',
+        message: 'What license would you use?',
+        name: 'license',
+        choices: ["MIT" , "Apache" , "N/A"]
+    },
+
+    {
+        type: 'input',
+        message: 'Are there any contributers?',
+        name: 'Contributing'
+    },
+
+    {
+        type: 'input',
+        message: 'Are there any tests?',
+        name: 'Tests'
+    },
+
+    {
+        type: 'input',
+        message: 'Are there any questions?',
+        name: 'Questions'
+    },
+
+    {
+        type: 'input',
+        message: 'Are there any organization or people you want to give credit for using their maetrials or resources?',
+        name: 'Credits'
+    },
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName , data) {
+
+    return fs.writeFile(path.join(process.cwd(), fileName), data, err => console.log(err));
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+    inquirer.prompt(questions)
+    .then((data) => {
+       
+        writeToFile('./ReadMeGeneratorFolder/README.md', generateMarkDown({...data}));
+
+});
+}
+
+// Function call to initialize app
+init();
